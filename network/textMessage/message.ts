4e014ad1856ac141e501026bc4d79c3075a3290f
zhaohappy <2281756061@qq.com>
2024-10-27 23:00:20 +0800

    feat: 添加类 http 请求模块

---
+++ network/textMessage/message.ts
@@ -0,0 +1,92 @@
+import IOReader from '../../io/IOReader'
+import IOWriter from '../../io/IOWriter'
+import * as object from '../../util/object'
+
+export class TextMessageRequest {
+  method: string
+  uri: string
+  protocol: string
+  headers: Record<string, string>
+  context: string
+
+  constructor(method: string, uri: string, protocol: string, headers: Record<string, string> = {}, context: string = '') {
+    this.method = method
+    this.uri = uri
+    this.protocol = protocol
+    this.headers = headers
+    this.context = context
+  }
+
+  public setHeader(key: string, value: string) {
+    this.headers[key] = value
+  }
+
+  public encode() {
+    let text = `${this.method} ${this.uri} ${this.protocol}\r\n`
+    this.headers['Content-Length'] = this.context.length + ''
+    object.each(this.headers, (value, key) => {
+      if (value) {
+        text += `${key}: ${value}\r\n`
+      }
+    })
+    text += '\r\n'
+
+    if (this.context) {
+      text += this.context
+    }
+    return text
+  }
+}
+
+export class TextMessageResponse {
+  protocol: string
+  statusCode: number
+  statusText: string
+  headers: Record<string, string>
+  context: string
+
+  constructor(protocol: string, statusCode: number, statusText: string, headers: Record<string, string>, context: string) {
+    this.protocol = protocol
+    this.statusCode = statusCode
+    this.statusText = statusText
+    this.headers = headers
+    this.context = context
+  }
+}
+
+export abstract class TextMessageSession {
+  protected ioReader: IOReader
+
+  protected ioWriter: IOWriter
+
+  constructor(ioReader: IOReader, ioWriter: IOWriter) {
+    this.ioReader = ioReader
+    this.ioWriter = ioWriter
+  }
+
+  protected async readResponse() {
+    let head = (await this.ioReader.readLine()).trim().split(' ')
+    let context = ''
+    let headers: Record<string, string> = {}
+    while (true) {
+      const line = (await this.ioReader.readLine()).trim()
+      // 响应头结束
+      if (!line) {
+        break
+      }
+      const item = line.split(':')
+      headers[item[0].trim()] = item[1].trim()
+    }
+    if (headers['Content-Length']) {
+      context = await this.ioReader.readString(+headers['Content-Length'])
+    }
+    return new TextMessageResponse(head[0].trim(), +head[1].trim(), head[2].trim(), headers, context)
+  }
+
+  public async request(request: TextMessageRequest): Promise<TextMessageResponse> {
+    this.ioWriter.reset()
+    this.ioWriter.writeString(request.encode())
+    this.ioWriter.flush()
+    return this.readResponse()
+  }
+}
