eb94f200b7fe9546f2c615c71915883e170ae97e
zhaohappy <2281756061@qq.com>
2025-06-13 15:45:15 +0800

    feat: 新增 array 判断两个数据是否相同的方法

--- util/array.ts
+++ util/array.ts
@@ -286,3 +286,21 @@ export function sortInsert<T>(array: T[], item: T, callback: (item: T) => -1 | 0
     array.push(item)
   }
 }
+
+export function same<T extends ArrayLike<any>>(a: T, b: T): boolean {
+  if (a === b) {
+    return true
+  }
+  if (!a || !b) {
+    return false
+  }
+  if (a.length !== b.length) {
+    return false
+  }
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false
+    }
+  }
+  return true
+}
